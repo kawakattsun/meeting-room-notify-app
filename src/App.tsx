033439eeb36a359f39be47ea41f04f9ef86df8ab
kawakattsun <riverwin.akira@gmail.com>
2020-02-09 18:29:19 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const wssEndpoint = process.env.WSS_ENDPOINT
+
+const App = () => {
+  const [availability, setAvailability] = useState('off')
+  
+  useEffect(() => {
+    const ws = new WebSocket(wssEndpoint)
+    ws.onopen = (event) => {
+      console.log('info: ws connected.')
+    }
+    ws.onmessage = (event) => {
+      const response = JSON.parse(event.data)
+      if (response.message === 'on' || response.message === 'off') {
+        setAvailability(response.message)
+      }
+    }
+
+    return () => {
+      ws.close()
+    };
+  }, []);
+
+  return (
+    <div className={`meetingRoomNotification meetingRoomNotification-${availability}`}>
+      <p>会議室 {availability === 'on' ? '使用中' : '空室'}</p>
+    </div>
+  );
+}
+
+export default App;
